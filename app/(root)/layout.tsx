cc13e55adee9ca283da31006ba4cc58b79ff1c2b
xyvie lyons <xyvielyons@gmail.com>
2024-08-22 04:16:53 +0300

    mobile navigation and full side bar navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,33 @@
+import Sidebar from '@/components/shared/Sidebar';
+import Image from 'next/image';
 import React from 'react'
+import MobileNav from '@/components/shared/MobileNav';
 const RootLayout = ({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) => {
+
+  const loggedIn = {firstName:'Adrian',lastName:'JSM'}
   return (
-    <main>
-    {children}
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedIn}></Sidebar>
+
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image
+          src="/icons/logo.svg"
+          alt='logo'
+          width={30}
+          height={30}
+          />
+          <div className="">
+            <MobileNav user={loggedIn}/>
+          </div>
+        </div>
+        {children}
+      </div>
+   
     </main>
   )
 }
