98252e1aa6827818ad11216494771af5b8a18961
xyvie lyons <xyvielyons@gmail.com>
2024-08-22 22:57:48 +0300

    integration with sentry api to log errors

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-
+import Image from 'next/image';
 const RootLayout = ({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) => {
   return (
-    <main>
+    <main className='flex min-h-screen w-full justify-between font-inter'>
     {children}
+    <div className="auth-asset">
+      <div className="">
+        <Image
+        src="/icons/auth-image.svg"
+        alt='Aith Image'
+        width={500}
+        height={500}
+        />
+      </div>
+    </div>
     </main>
   )
 }
